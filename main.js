f4563fd765e41f2c6aa7e484cc98823246eac655
Xordas <120686481+Xordas@users.noreply.github.com>
2025-03-07 22:12:29 +0000

    improve settings page and fix about blank cloak

--- main.js
+++ main.js
@@ -148,7 +148,12 @@ if (cluster.isPrimary) {
     res.setHeader('X-Content-Type-Options', 'nosniff');
     res.setHeader('X-XSS-Protection', '1; mode=block');
     res.setHeader('Referrer-Policy', 'no-referrer');
-    res.setHeader('X-Frame-Options', 'DENY');
+    res.setHeader(
+      'Content-Security-Policy', 
+      "frame-ancestors 'self' null about:blank;"
+    );
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    
     next();
   });
 
